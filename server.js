6b6a834389629fd63c102decc1aef35078460670
Bharti <poonampoonam5186@email.com>
2026-04-16 09:45:43 +0530

    added cors and fixed port

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const multer = require('multer')
@@ -6,6 +7,7 @@ const path = require('path')
 const db = require('./database')
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use(express.static('public'))
 
@@ -102,6 +104,8 @@ app.get('/files', tokenCheck, (req, res) => {
     res.json(files)
 })
 
-app.listen(3000, () => {
-    console.log('Server chalu ho gaya! Port 3000 pe')
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Server chalu ho gaya! Port ${PORT} pe`)
 })
